068c8eab483345a563d079b6cf40878161b8bf00
nathanm5266 <nathanm5266@gmail.com>
2021-08-03 00:47:27 +0300

    statements

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* 
+prints 3 statements in 3 different lines
+*/
+const cF = 'C is fun';
+const pC = 'Python is cool';
+const jA = 'Javascript is amazing';
+console.log(cF);
+console.log(pC);
+console.log(jA);
